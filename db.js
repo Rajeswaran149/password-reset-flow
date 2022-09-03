4f8e5eaee3457af3b4da4e1ae4e1d89527e86ecd
Rajeswaran149 <rajeswaran149@gmail.com>
2022-09-03 09:55:30 +0530

    password-reset

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const mongoURL = "mongodb+srv://rajeswaran:WQlO2zYL8aK1vtfO@cluster0.eutso4r.mongodb.net/password-reset?retryWrites=true&w=majority"
+
 module.exports = connection = async () => {
     try {
         const connectionParams = {
@@ -7,7 +9,7 @@ module.exports = connection = async () => {
             useCreateIndex: true,
             useUnifiedTopology: true,
         };
-        await mongoose.connect(process.env.DB, connectionParams);
+        await mongoose.connect(mongoURL, connectionParams);
         console.log("connected to database.");
     } catch (error) {
         console.log(error, "could not connect database.");
